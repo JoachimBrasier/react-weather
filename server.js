934bfd6adf65691510897e544af18a3c61d76226
Joachim <jo-brasier@outlook.com>
2019-02-14 20:40:59 +0100

    update production

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const app = express();
 
-if (process.env.NODE_END === 'production'){
+if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-};
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
