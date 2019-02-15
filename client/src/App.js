08c67cc2a65e56ab06bd11f76e344fc26e6a71d4
IcezLP <jo-brasier@outlook.com>
2019-02-15 11:56:20 +0100

    suppression helmet, compression image

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import store  from './store';
-import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
@@ -13,19 +12,6 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Helmet>
-          <meta name="description" content="Météo construite avec OpenWeatherMap, Bulma et React par Joachim Brasier" />
-          <meta property="og:title" content="Météo" />
-          <meta property="og:url" content="https://meteo.jbrasier.fr/" />
-          <meta property="og:description" content="Météo construite avec OpenWeatherMap, Bulma et React par Joachim Brasier" />
-          <meta property="og:image" content="https://meteo.jbrasier.fr/og.svg" />
-          <meta property="og:image:secure_url" content="https://meteo.jbrasier.fr/og.svg" />
-          <meta property="og:image:alt" content="Météo" />
-          <meta property="og:image:type" content="image/svg" />
-          <meta property="og:site_name" content="Météo" />
-          <meta property="og:locale" content="fr_FR" />
-          <link rel="canonical" href="https://meteo.jbrasier.fr" />
-        </Helmet>
         <Router>
           <div className={'app'}>
             <Route exact path={'/'} component={Weather} />
