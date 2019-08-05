a85227466fd052e2da4ce5cdf82289969917fdb6
Joachim <jo-brasier@outlook.com>
2019-08-05 19:20:03 +0200

    Updates

---
+++ precache-manifest.7ac2bd9607b80d64cc88a3868cf5b872.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78be78e8bf07f12a1803839a785306df",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "961bfb4acfaf5441f57d",
+    "url": "/react-weather/static/css/2.2687cd84.chunk.css"
+  },
+  {
+    "revision": "954d3a19d27e1eb373af",
+    "url": "/react-weather/static/css/main.7c5e272b.chunk.css"
+  },
+  {
+    "revision": "961bfb4acfaf5441f57d",
+    "url": "/react-weather/static/js/2.e96f744a.chunk.js"
+  },
+  {
+    "revision": "954d3a19d27e1eb373af",
+    "url": "/react-weather/static/js/main.1d92c9a7.chunk.js"
+  },
+  {
+    "revision": "e61e452087af7072f6e9",
+    "url": "/react-weather/static/js/runtime~main.2302d1fd.js"
+  },
+  {
+    "revision": "e6656612121b4920a786d3e5c603bac9",
+    "url": "/react-weather/static/media/background.e6656612.jpg"
+  },
+  {
+    "revision": "ff09edbebdae84e027d34ce692706e05",
+    "url": "/react-weather/static/media/background2.ff09edbe.jpg"
+  },
+  {
+    "revision": "a2e6c8283b831ad53f96cef7b099e32b",
+    "url": "/react-weather/static/media/loading.a2e6c828.svg"
+  }
+]);
