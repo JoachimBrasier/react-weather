6162ccd304e7ac77d1e706910694052b76eaa4eb
Joachim <jo-brasier@outlook.com>
2019-08-05 19:42:48 +0200

    remove react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import store  from './store';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import store from './store';
 import { Provider } from 'react-redux';
+import Weather from './components/weather/Weather';
 
 import './app.css';
-import 'bulma/css/bulma.min.css'
-
-import Weather from './components/weather/Weather';
+import 'bulma/css/bulma.min.css';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <div className={'app'}>
-            <Route exact path={'/'} component={Weather} />
-          </div>
-        </Router>
+        <Weather />
       </Provider>
     );
-  };
-};
+  }
+}
 
 export default App;
